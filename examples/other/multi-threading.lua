959f4b3d22b61368f01b7bd60c32d2a31aa26f26
osch <oliver at luced de>
2018-03-30 16:24:05 +0200

    multi-threading support & example

---
+++ examples/other/multi-threading.lua
@@ -0,0 +1,80 @@
+#!/usr/bin/env lua
+-- MoonFLTK example: multi-threading.lua
+
+-- Demonstrates fl.await and fl.thread_message together with
+-- the LuaLanes multi-threading library (https://github.com/LuaLanes/lanes).
+
+local lanes = require"lanes".configure()
+local fl    = require"moonfltk"
+
+fl.lock() -- "start" the FLTK lock mechanism
+          -- this is needed for initializing fl.await and fl.thread_message
+
+local START_NUMBER = 15
+local DEBUG        = false
+local TRACE        = DEBUG and print or function() end
+
+local linda1 = lanes.linda()
+
+function background_code(threadName, initialSleepSeconds)
+    -- This function together with simple upvalues is serialized and 
+    -- transfered to another thread, see the LuaLanes documentation 
+    -- for more details: http://lualanes.github.io/lanes/
+    TRACE(threadName.." started")
+    local lanes = require"lanes"    -- module cannot be serialized
+    local fl    = require"moonfltk" -- module cannot be serialized
+    lanes.sleep(initialSleepSeconds)
+    for i= START_NUMBER, 0, -1 do
+        linda1:send(threadName, i)
+        fl.awake("awake from " .. threadName)
+        if i > 0 then
+            lanes.sleep(1)
+        end
+    end
+    TRACE(threadName.." sending quit")
+    linda1:send("quit", threadName)
+    fl.awake("awake from " .. threadName)
+end
+
+local createThread = lanes.gen("*", background_code)
+
+local thread1 = createThread("thread1", 2)
+local thread2 = createThread("thread2", 2.5)
+
+local win = fl.window(440, 180, arg[0])
+local box = fl.box(20, 40, 400, 100, "Hello, World!");
+
+box:box('up box')
+box:labelfont(fl.BOLD)
+box:labelsize(12)
+box:align(fl.ALIGN_INSIDE | fl.ALIGN_LEFT | fl.ALIGN_TOP | fl.ALIGN_WRAP)
+win:done()
+win:show(arg[0], arg)
+
+local m1 = ""
+local m2 = ""
+local finished = {}
+
+while fl.wait() do
+    local m = fl.thread_message()
+    if m then
+        TRACE("thread_message: " , m)
+        local k, v = linda1:receive(0, "thread1", "thread2", "quit")
+        TRACE("received from linda:", k, v)
+        if k == "thread1" then
+            m1 = v
+        elseif k == "thread2" then
+            m2 = v
+        elseif k == "quit" then
+            finished[v] = true
+        end
+        local label = "Hello, World!\n - Received from thread1: " .. m1 .. ","
+                                .. "\n - Received from thread2: " .. m2 .. "."
+        if finished.thread1 and finished.thread2 then
+            label = label .. "\nAll threads are finished!"
+        end
+        box:label(label)
+    else
+        TRACE("something happend in FLTK")
+    end
+end
