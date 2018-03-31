9de2fd4839ecf1485e7219adb731b9e6ef65a24e
osch <oliver at luced de>
2018-03-31 14:55:21 +0200

    queue multiple thread messages

--- examples/other/multi-threading.lua
+++ examples/other/multi-threading.lua
@@ -16,24 +16,24 @@ local TRACE        = DEBUG and print or function() end
 
 local linda1 = lanes.linda()
 
-function background_code(threadName, initialSleepSeconds)
+local function background_code(threadName, initialSleepSeconds)
     -- This function together with simple upvalues is serialized and 
     -- transfered to another thread, see the LuaLanes documentation 
     -- for more details: http://lualanes.github.io/lanes/
-    TRACE(threadName.." started")
+    TRACE(threadName..": started.")
     local lanes = require"lanes"    -- module cannot be serialized
     local fl    = require"moonfltk" -- module cannot be serialized
     lanes.sleep(initialSleepSeconds)
-    for i= START_NUMBER, 0, -1 do
+    for i = START_NUMBER, 0, -1 do
         linda1:send(threadName, i)
         fl.awake("awake from " .. threadName)
         if i > 0 then
             lanes.sleep(1)
         end
     end
-    TRACE(threadName.." sending quit")
+    TRACE(threadName..": sending quit")
     linda1:send("quit", threadName)
-    fl.awake("awake from " .. threadName)
+    fl.awake("awake from " .. threadName.." for quit")
 end
 
 local createThread = lanes.gen("*", background_code)
@@ -55,26 +55,43 @@ local m1 = ""
 local m2 = ""
 local finished = {}
 
+TRACE("main: starting event loop.")
 while fl.wait() do
-    local m = fl.thread_message()
-    if m then
-        TRACE("thread_message: " , m)
+    TRACE("main: ------------- event loop activated -------------")
+
+    -- Read all thread messages.
+    -- If fl.awake was called with an optional message string,
+    -- tl.thread_message must be called to remove message
+    -- strings from queue. This is not necessary, if
+    -- fl.awake is called without argument.
+    -- In this example the thread message is only used
+    -- for demonstration.
+    repeat
+        local m = fl.thread_message()
+        if m then
+            TRACE("main: thread_message: " , m)
+        end
+    until m == nil
+
+    -- Read all linda events.
+    repeat    
         local k, v = linda1:receive(0, "thread1", "thread2", "quit")
-        TRACE("received from linda:", k, v)
-        if k == "thread1" then
-            m1 = v
-        elseif k == "thread2" then
-            m2 = v
-        elseif k == "quit" then
-            finished[v] = true
+        if k then
+            TRACE("main: received from linda:", k, v)
+            if k == "thread1" then
+                m1 = v
+            elseif k == "thread2" then
+                m2 = v
+            elseif k == "quit" then
+                finished[v] = true
+            end
+            local label = "Hello, World!\n - Received from thread1: " .. m1 .. ","
+                                    .. "\n - Received from thread2: " .. m2 .. "."
+            if finished.thread1 and finished.thread2 then
+                label = label .. "\nAll threads are finished!"
+            end
+            box:label(label)
         end
-        local label = "Hello, World!\n - Received from thread1: " .. m1 .. ","
-                                .. "\n - Received from thread2: " .. m2 .. "."
-        if finished.thread1 and finished.thread2 then
-            label = label .. "\nAll threads are finished!"
-        end
-        box:label(label)
-    else
-        TRACE("something happend in FLTK")
-    end
+    until k == nil
 end
+TRACE("main: event loop finished.")
